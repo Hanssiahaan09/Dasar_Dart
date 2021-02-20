011d0ff2f777b37dc5bcb131fc9ce772c19e8e8d
Hanssiahaan09 <78518083+Hanssiahaan09@users.noreply.github.com>
2021-02-20 11:24:06 +0700

    Create variabel.dart

---
+++ variabel.dart
@@ -0,0 +1,12 @@
+void main() {
+  String nama = "Hans";
+  int umur = 20;
+  double tinggi = 160,4;
+  bool isMenikah = false;
+
+  var alamat = "Medan, Indonesia";
+
+  print("Nama saya $nama. Umur $umur tahun. Tinggi sekitar $tinggi cm.");
+  print("Menikah: $isMenikah");
+  print("Alamat : $alamat");
+}
